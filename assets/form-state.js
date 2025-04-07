e3f5b157cbc457e300f713af0df5cbabd5bced35
Amely Richter <richa817@newschool.edu>
2025-04-07 15:21:06 -0400

    rendering all the information

--- assets/form-state.js
+++ assets/form-state.js
@@ -1,3 +1,5 @@
+let formdisplay = []
+
 document.querySelector('form').onsubmit = (event) => event.preventDefault()
 document.querySelector('form').oninput = (event) => {
 	
@@ -10,4 +12,34 @@ document.querySelector('form').oninput = (event) => {
 	let date = document.querySelector('#date').value
 	document.querySelector('#date-display').textContent = exhibition 
 	// this probably need to be something else than text content since its a date
+}
+
+document.querySelector('form').onsubmit = (event) => {
+	event.preventDefault()
+
+	let nameValue = document.querySelector('#name').value
+	let exhibitionValue = document.querySelector('#exhibition').value
+	let dateValue = document.querySelector('#date').value
+
+	let nameObject = {
+		name: nameValue,
+		text: exhibitionValue,
+		date: dateValue,
+	}
+
+	formdisplay.push(nameObject) 
+	renderFormdisplay()
+
+	const renderFormdisplay = () => {
+		let formdisplayList = document.querySelector('#formdisplay')
+		formdisplayList.innerHTML = ''
+
+		formdisplay.forEach((nameObject) => {
+			let nameItem = `
+				<li><h2>${nameObject.name}</h2></li>
+				<li><h2>${nameObject.text}</h2></li>
+				<li><h2>${nameObject.date}</h2></li>
+			`
+		})
+	}
 }
