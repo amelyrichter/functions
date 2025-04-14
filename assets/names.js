6b31184d8f4417230adb465dfd8edc75a0084dda
Amely Richter <richa817@newschool.edu>
2025-04-14 15:12:34 -0400

    styling my input

--- assets/names.js
+++ assets/names.js
@@ -7,10 +7,21 @@ if (!formdisplay) {
 
 formdisplay.forEach((name) => {
 	let nameItem = `
-		<li><h2>${name.name}</h2></li>
+	<div class="countdown-box">
+	<div class="top-row">
+		<li><h2 class="title-left">${name.name}</h2></li>
+		<li><h2 class="title-right">${name.date}</h2></li>
+	</div>
+
+	<div class="countdown-content">
+		<li><h2 class="countdown-number">${name.daysLeft}</h2></li>
+		<div class="days-left">days left</div>
+	</div>
+
+	<div class="bottom-title">
 		<li><h2>${name.description}</h2></li>
-		<li><h2>${name.date}</h2></li>
-		<li><h2>${name.daysLeft} days left</h2></li>
+	</div>
+		
 	`
 	document.querySelector('#formdisplay').insertAdjacentHTML('beforeend', nameItem)
 })
